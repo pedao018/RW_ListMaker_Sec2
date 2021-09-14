fa279385f416b9547f01cbf6bc591f16857e1255
HAIND <HAIND haind@hqsoft.com.vn>
2021-09-14 23:18:14 +0700

    Create Empty Activity

---
+++ app/src/main/java/com/raywenderlich/rw_listmaker_sec2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.raywenderlich.rw_listmaker_sec2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
