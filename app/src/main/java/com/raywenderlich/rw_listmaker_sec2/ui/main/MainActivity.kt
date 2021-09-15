0160699065a169ce3d534284f7b07f4ff0e1cc79
HAIND <HAIND haind@hqsoft.com.vn>
2021-09-15 23:07:15 +0700

    Step 1

--- app/src/main/java/com/raywenderlich/rw_listmaker_sec2/ui/main/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/rw_listmaker_sec2/ui/main/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.raywenderlich.rw_listmaker_sec2
+package com.raywenderlich.rw_listmaker_sec2.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.raywenderlich.rw_listmaker_sec2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
     }
 }
